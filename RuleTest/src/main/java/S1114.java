cdef5d3afd89dce3822f114b91d9dc1e82beaa8d
Alexandre Monteiro Janoni <alexandre.janoni@aurea.com>
2019-05-02 14:21:49 -0600

    rule test

--- RuleTest/src/main/java/S1114.java
+++ RuleTest/src/main/java/S1114.java
@@ -1,7 +1,7 @@
 public class S1114 {
     protected void finalize() throws Throwable {
         try {
-            // no method
+            ;;// no method
         } catch (Throwable t) {
             releaseSomeResources();
             super.finalize(); // non-compliant- Super is at the end of the
@@ -14,12 +14,5 @@ public class S1114 {
 
     }
 
-    public void test() {
-
-    }
-
-    public void test2() {
-
-    }
 }
 
